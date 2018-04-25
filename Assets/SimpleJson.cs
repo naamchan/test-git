833d8953620a256b914ffc71b22f99a0819abca2
Thara Mongkolwannalert <naamchan@gmail.com>
2018-04-25 15:38:46 +0700

    Add simple json

---
+++ Assets/SimpleJson.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class PersonalInfo
+{
+    public string first_name;
+    public int age;
+    public Company company;
+    public ProgrammingLanguage[] programming_languages;
+}
+
+[Serializable]
+public class ProgrammingLanguage
+{
+    public string name;
+    public int level;
+}
+
+[Serializable]
+public class Company
+{
+    public string name;
+    public string position;
+}
+
+public class SimpleJson : MonoBehaviour
+{
+    [SerializeField]
+    private string jsonString; //{"first_name": "Thara","company": {"name": "True Axion","position": "Senior programmer"},"age": 30,"programming_languages": [{"name": "C","level": 3},{"name": "C++","level": 4},{"name": "C#","level": 5}]}
+
+    void Start()
+    {
+        // Deserialize
+        PersonalInfo personalInfo = JsonUtility.FromJson<PersonalInfo>(jsonString);
+
+        Debug.Log("Age :" + personalInfo.age);
+    }
+}
